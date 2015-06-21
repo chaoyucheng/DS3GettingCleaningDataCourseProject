474b3b9fbc2ed1773a6de345e5fa4b65fcc48f76
Chao-Yu Cheng <chaoyucheng001@gmail.com>
2015-06-21 09:12:03 -0400

    Signed-off-by: Chao-Yu Cheng <chaoyucheng001@gmail.com>

--- run_analysis.R
+++ run_analysis.R
@@ -9,8 +9,8 @@ run_analysis <- function () {
   y_train <- read.table(paste(data_path, "train/y_train.txt", sep=""))
   colnames(x_test) <- features[,2]
   colnames(x_train) <- features[,2]
-  x_test_mean_std <- x_test[grepl("tBodyAcc-mean\\(|tBodyAcc-std|tBodyGyro-mean\\(|tBodyGyro-std", names(x_test))]
-  x_train_mean_std <- x_train[grepl("tBodyAcc-mean\\(|tBodyAcc-std|tBodyGyro-mean\\(|tBodyGyro-std", names(x_train))]
+  x_test_mean_std <- x_test[grepl("BodyAcc-mean\\(|BodyAcc-std|BodyGyro-mean\\(|BodyGyro-std", names(x_test))]
+  x_train_mean_std <- x_train[grepl("BodyAcc-mean\\(|BodyAcc-std|BodyGyro-mean\\(|BodyGyro-std", names(x_train))]
   x_mean_std <- rbind(x_test_mean_std, x_train_mean_std)
   y_mean_std <- rbind(y_test, y_train)
   y_mean_std_acty_name <- vector(mode="character", length = nrow(y_mean_std))
@@ -26,33 +26,35 @@ run_analysis <- function () {
   colnames(avg_data_set) <- c("Activity", colnames(x_mean_std))
   
   ## Melt dimention X, Y, Z
-  dimx <- avg_data_set[,c(1,2,5,8,11)]
-  dimy <- avg_data_set[,c(1,3,6,9,12)]
-  dimz <- avg_data_set[,c(1,4,7,10,13)]
+  dimx <- avg_data_set[,c(1,2,5,8,11,14,17,20,23)]
+  dimy <- avg_data_set[,c(1,3,6,9,12,15,18,21,24)]
+  dimz <- avg_data_set[,c(1,4,7,10,13,16,19,22,25)]
   dimx <- cbind(dimx, c("X"))
   dimy <- cbind(dimy, c("Y"))
   dimz <- cbind(dimz, c("Z"))
   data_set_dim <- rbind(dimx, dimy, dimz)
-  colnames(data_set_dim) <- c("Activity", "Value.Body.Acc.Mean", "Value.Body.Acc.Std", "Value.Body.Gyro.Mean", "Value.Body.Gyro.Std", "Dimention")
+  colnames(data_set_dim) <- c("Activity", "Value.Body.Acc.Mean.Time", "Value.Body.Acc.Std.Time", "Value.Body.Gyro.Mean.Time", "Value.Body.Gyro.Std.Time", "Value.Body.Acc.Mean.Freq", "Value.Body.Acc.Std.Freq", "Value.Body.Gyro.Mean.Freq", "Value.Body.Gyro.Std.Freq", "Dimention")
   
   ## Melt measure type Mean or Std
-  measure_mean <- data_set_dim[, c(1,2,4,6)]
-  measure_std <- data_set_dim[, c(1,3,5,6)]
+  measure_mean <- data_set_dim[, c(1, 2, 4, 6, 8, 10)]
+  measure_std <- data_set_dim[, c(1, 3, 5, 7, 9, 10)]
   measure_mean <- cbind(measure_mean, c("Mean"))
   measure_std <- cbind(measure_std, c("Standard Deviation"))
   data_set_dim_measure <- rbind(measure_mean, measure_std)
-  colnames(data_set_dim_measure) <- c("Activity", "Value.Body.Acc", "Value.Body.Gyro", "Dimention", "Statistic.Measure")
+  colnames(data_set_dim_measure) <- c("Activity", "Value.Body.Acc.Time", "Value.Body.Gyro.Time", "Value.Body.Acc.Freq", "Value.Body.Gyro.Freq", "Dimention", "Statistic.Measure")
   
   ## Melt sensor name Acc or Gyro
-  sensor_acc <- data_set_dim_measure[, c(1, 2, 4, 5)]
-  sensor_gyro <- data_set_dim_measure[, c(1, 3, 4, 5)]
+  sensor_acc <- data_set_dim_measure[, c(1, 2, 4, 6, 7)]
+  sensor_gyro <- data_set_dim_measure[, c(1, 3, 5, 6, 7)]
   sensor_acc <- cbind(sensor_acc, c("Accelerometer"))
   sensor_gyro <- cbind(sensor_gyro, c("Gyroscope"))
   data_set_dim_measure_sensor <- rbind(sensor_acc, sensor_gyro)
-  colnames(data_set_dim_measure_sensor) <- c("Activity", "Avg.Body.Signal.Value", "Dimention", "Statistic.Measure", "Sensor.Type")
+  colnames(data_set_dim_measure_sensor) <- c("Activity", "Avg.Body.Signal.Value.Time", "Avg.Body.Signal.Value.Freq", "Dimention", "Statistic.Measure", "Sensor.Type")
+  
+  ## Final process of the data set
   df_data_set_dim_measure_sensor <- as.data.frame(data_set_dim_measure_sensor)
   df_data_set_dim_measure_sensor <- mutate(df_data_set_dim_measure_sensor, Activity.Name=as.character(activity_labels[Activity, 2]))
-  tidy_data_set_dim_measure_sensor <- df_data_set_dim_measure_sensor[, 2:6]
+  tidy_data_set_dim_measure_sensor <- df_data_set_dim_measure_sensor[, 2:7]
   write.table(tidy_data_set_dim_measure_sensor, "tidy_data_set_dim_measure_sensor.txt", row.name=FALSE)
   tidy_data_set_dim_measure_sensor
 }
